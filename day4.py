795bcacdb5719288d6d6a14c1d0be40f49470815
Amal Sebastian <83443068+amalsebastian7@users.noreply.github.com>
2022-03-10 23:49:34 +0530

    4.5

---
+++ day4.py
@@ -0,0 +1,36 @@
+#____________________________Day 4____________________________________________
+#import 
+# use import as header to connect different modules .
+# import random 
+# import some-local-module 
+
+
+import random
+rand=random.random()
+print(f"random number is {rand}")
+
+rand1=random.randomint(1,10)
+print(f"random number is {rand1}") 
+
+#____________________coin toss program____________________________
+
+import random
+test=int(input("Create a seed number: "))
+random.randint(0,1)
+if random==1:
+    print("Heads")
+else:
+    print("Tails")
+    
+#______________________________________________________________________
+#There are lists that are a type of data structure which can store lots of data as in a list
+#List have an ordered format .
+
+states=["kerala","tamilnadu","karnataka","goa","jammu","kashmir","andra"]
+#here the one in [0] is kerala and [1] is tamilnadu ,[2] is karnataka,[-1] is andra
+
+state.append("delhi")#use append to add an item to the end of the list.{use extend instead of append to add a list to another list}
+
+ 
+
+
